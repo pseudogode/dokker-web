a65371afa287c07548e1a9262ea33cd2f476bf58
@pseudogode <@pseudogode>
2025-02-10 21:23:08 +0000

    Add login script

---
+++ frontend/login/index.js
@@ -0,0 +1,58 @@
+const SUCCESS = 'success';
+const ERROR = 'error';
+const TOKEN_KEY = 'token';
+
+const onSubmitHandler = (form, formContainer) => async (event) => {
+  event.preventDefault();
+  const data = new FormData(event.target);
+  const formDataObject = Object.fromEntries(data);
+  
+  try {
+    const res = await fetch('../../backend/login.php', {
+      method: 'POST',   
+      headers: {
+          'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(formDataObject),
+    });
+
+    const { status, message, token } = res.json();
+
+    if (status === SUCCESS) {
+      localStorage.setItem(TOKEN_KEY, token);
+      setTimeout(() => {
+        window.location.href = '../dashboard/index.html'; 
+      }, 2000);
+      return;
+    }
+
+    if (status === ERROR) {
+      const messageContainerId = 'message-container';
+      let messageContainer = document.getElementById(messageContainerId);
+      if (!messageContainer) {
+        messageContainer = document.createElement('div');
+        messageContainer.setAttribute('id', messageContainerId);
+        formContainer.appendChild(messageContainer);
+      }
+
+      let messageElement = messageContainer.children[0];
+      if (!messageElement) {
+        messageElement = document.createElement('p');
+      }
+
+      messageElement.innerText = message;
+      form.reset();
+      return;
+    }
+
+    throw new Error('Unexpected server response', status);
+
+  } catch(error) {
+    console.trace(error, 'Error on submitting form data');
+  }
+}
+
+const form = document.getElementById('login-form');
+const formContainer = document.getElementById('form-container');
+
+loginForm.addEventListener('submit', onSubmitHandler(form, formContainer));
