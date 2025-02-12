77d783ffe64440fadd06f3aa947292baa1e22724
@pseudogode <@pseudogode>
2025-02-12 21:40:23 +0000

    Add simple container visualisation mock

--- frontend/dashboard/index.js
+++ frontend/dashboard/index.js
@@ -1,9 +1,24 @@
 import { renderRegisterAndLoginButtons, renderLogOutButton } from '../shared/header.js';
 import { authWrapper } from '../shared/auth.js';
-import { getAllContainersMock } from '../mock/getAllContainers.js';
+import { getAllContainersMock, mapContainerStateToClass } from '../mock/getAllContainers.js';
 
 const NAVIGATION_CONTAINER_ID = 'navigation-container';
 const MAIN_SECTION_ID = 'main-section';
+
+const renderDContainersList = (containerElement) => {
+  getAllContainersMock().map(({Names, Id, Image, Status, State }) => {
+    const pill = document.createElement('div');
+    pill.classList.add('pill', 'container-pill');
+    pill.innerHTML = `
+      <strong class="container-prop">${Names[0]}</strong>
+      <span class="container-prop image">${Image}</span>
+      <span class="pill container-prop ${mapContainerStateToClass(State)}">${State}</span>
+      <span class="status container-prop">${Status}</span>
+    `;
+
+    containerElement.appendChild(pill);
+  });
+}
 
 const renderDashboard = async () => {
   const navigationContainer = document.getElementById(NAVIGATION_CONTAINER_ID);
@@ -19,6 +34,7 @@ const renderDashboard = async () => {
       () => {
         renderLogOutButton(navigationContainer);
         mainSection.innerHTML='<h1>Dashboard<h1>';
+        renderDContainersList(mainSection);
       },
   );
 }
