bb3f41f3709ea6e6c751852a0c93305632064ee1
Stefan Genadiev <stefangenadiev@Stefans-MacBook-Pro.local>
2025-02-13 12:59:04 +0200

    fix

--- backend/api/docker_api/containers.php
+++ backend/api/docker_api/containers.php
@@ -3,12 +3,12 @@ require_once __DIR__ . '/../../utils/constants.php';
 require_once __DIR__ . '/../../utils/utils.php';
 require_once __DIR__ . '/DockerClient.php';
 
-if ($_SERVER["REQUEST_METHOD"] != "POST") {
-    jsonResponse([
-      'status' => ERROR,
-      'message' => 'Method not allowed',
-    ], 400);
-}
+// if ($_SERVER["REQUEST_METHOD"] != "POST") {
+//     jsonResponse([
+//       'status' => ERROR,
+//       'message' => 'Method not allowed',
+//     ], 400);
+// }
 
 $dockerClient = new DockerClient();
 
